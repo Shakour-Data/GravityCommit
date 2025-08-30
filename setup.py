3fb02d2da6e53e232995aef8ef82e43c8f8dfff7
Shakour <81189779+Shakour-Data@users.noreply.github.com>
2025-08-30 16:44:34 +0000

    📝 Update: Add Python source: setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gravitycommit",
+    version="1.0.0",
+    author="BlackBoxAI",
+    author_email="",
+    description="Automatic commit package for Python projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/gravitycommit",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        "gitpython>=3.1.0",
+        "schedule>=1.1.0",
+        "psutil>=5.8.0",
+        "click>=8.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "autocommit=autocommit.cli:main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
