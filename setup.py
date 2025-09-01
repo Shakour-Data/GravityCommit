2f93aa87b258095023a03ea0503afa4b7ccf2469
Shakour <81189779+Shakour-Data@users.noreply.github.com>
2025-09-01 09:06:04 +0000

    chore: bump version to 1.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="gravitycommit",
-    version="1.5.0",
+    version="1.5.1",
     author="BlackBoxAI",
     author_email="",
     description="Automatic commit package for Python projects",
